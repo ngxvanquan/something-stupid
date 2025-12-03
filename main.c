74786cce43e110124938109843463891d019f300
haruto <ngxvanquan@gmail.com>
2025-12-03 22:33:16 +0700

    upload

--- main.c
+++ main.c
@@ -1,6 +1,12 @@
 #include <stdio.h>
 
-int main() {
-	printf("Hello World\n");
+int main() {	
+	long myNumbers[] = {123452345, 5, 3, 6, 3, 5};
+	int arrayLength = sizeof(myNumbers) / 4;
+	printf("%l\n", myNumbers[1]);
+	printf("size of this array is %zu\n", sizeof myNumbers);
+	printf("size of first element array is %zu\n", sizeof myNumbers[0]);
+	printf("length of array is: %d\n", arrayLength);
+
 	return 0;
 }
--- something.js
+++ something.js
@@ -1,13 +1,4 @@
 
-// const studentName = document.querySelector(".student-name");
-// const timeCompleted = document.getElementById("timeCompleted");
-
-// studentName.addEventListener("click", () => {
-
-// 	let d = new Date();
-//   	timeCompleted.textContent = "Thời gian hoàn thành 17:" + (d.getMinutes() + 2) + ":" + d.getSeconds()  + " ngày 25/11/2025";	
-// });
-
 const studentName = document.querySelector(".student-name");
 const timeCompleted = document.getElementById("timeCompleted");
 
